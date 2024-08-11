a16e2003df8fe3369110c8baf4d21908ec76bfc3
Monira Akter <akter.monira.cse@gmail.com>
2024-08-11 23:07:36 +0600

    while loop

---
+++ while_loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{ 
+        int i;
+    i = 1;
+    while(i <= 100)
+    {
+        cout<< i<<endl;
+        i++;
+    }
+    cout << "End the numbers";
+    getch();
+
+}
+
